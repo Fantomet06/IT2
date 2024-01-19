edefc01f5e2a4e21979cf2f57146ea3fc44e6522
Fantomet <44312327+Fantomet06@users.noreply.github.com>
2024-01-19 15:04:11 +0100

    update

--- 3/3B/oppgave3.py
+++ 3/3B/oppgave3.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
 import csv
+print("\n")
 
-with open("./3/boliger.csv", encoding="utf-8-sig", mode="r") as f:
-    statistikk = csv.reader(f, delimiter="	") # delimiter ble endra pga excel
+with open("./3/3B/boliger.csv", encoding="utf-8-sig", mode="r") as f:
+    statistikk = csv.reader(f, delimiter=";") 
     x = next(statistikk) # linje 1 som liste
     y = next(statistikk) # linje 2 som liste
 
@@ -10,7 +11,11 @@ tittel1 = str(x[0])
 tittel2 = str(y[0])
 x.pop(0)
 y.pop(0)
-y = [int(i) for i in y]
+
+above_1000 = [int(y[i]) for i in range(len(y)) if len(x[i]) >= 11]
+y = [int(y[i]) if int(y[i]) not in above_1000 else sum(above_1000) for i in range(len(y))]
+x = [x[i] if len(x[i]) < 11 else "Over 1000m" for i in range(len(y))]
+
 plt.barh(x, y)
 plt.xlabel(tittel2)
 plt.ylabel(tittel1)
