cd2f43f19229bca35bc5b458777d1eae42f2f3d4
Fantomet <44312327+Fantomet06@users.noreply.github.com>
2024-01-23 13:12:27 +0100

    minor fix 2

--- 3/3B/oppgave3.py
+++ 3/3B/oppgave3.py
@@ -3,17 +3,14 @@ import csv
 
 with open("./3/3B/boliger.csv", encoding="utf-8-sig", mode="r") as f:
     statistikk = csv.reader(f, delimiter=";") 
-    x = next(statistikk) # linje 1 som liste
-    y = next(statistikk) # linje 2 som liste
+    x, y = next(statistikk), next(statistikk) # linje 1 som liste
 
-tittel1 = x.pop(0)
-tittel2 = y.pop(0)
+plt.xlabel(y.pop(0))
+plt.ylabel(x.pop(0))
 
 above_1000 = [int(y[i]) for i in range(len(y)) if len(x[i]) >= 11]
 y = [int(y[i]) if int(y[i]) not in above_1000 else sum(above_1000) for i in range(len(y))]
 x = [x[i] if len(x[i]) < 11 else "Over 1000m" for i in range(len(y))]
 
 plt.barh(x, y)
-plt.xlabel(tittel2)
-plt.ylabel(tittel1)
 plt.show()
