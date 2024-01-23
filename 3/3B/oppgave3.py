16e8ee756e1b8c6460d47f78af88955dfc3b4ff5
Fantomet <44312327+Fantomet06@users.noreply.github.com>
2024-01-23 12:56:28 +0100

    minor fix

--- 3/3B/oppgave3.py
+++ 3/3B/oppgave3.py
@@ -6,10 +6,8 @@ with open("./3/3B/boliger.csv", encoding="utf-8-sig", mode="r") as f:
     x = next(statistikk) # linje 1 som liste
     y = next(statistikk) # linje 2 som liste
 
-tittel1 = str(x[0])
-tittel2 = str(y[0])
-x.pop(0)
-y.pop(0)
+tittel1 = x.pop(0)
+tittel2 = y.pop(0)
 
 above_1000 = [int(y[i]) for i in range(len(y)) if len(x[i]) >= 11]
 y = [int(y[i]) if int(y[i]) not in above_1000 else sum(above_1000) for i in range(len(y))]
