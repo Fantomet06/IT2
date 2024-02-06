9af43b7255d0ae82854fa01da4e3e6bf9c7a2270
Fantomet <44312327+Fantomet06@users.noreply.github.com>
2024-02-06 15:22:14 +0100

    sdf
    asf

--- vurdering/csv/out/panda.py
+++ vurdering/csv/out/panda.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 data = pd.read_csv("./datasett/Medier.csv", index_col = 0, skiprows = (0, 1), \
-sep =";", na_values=[".", ".."], encoding = "latin-1")
+sep =";", na_values=[".", ".."], encoding = "utf-8-sig")
 
 data.columns = range(2010,2020)
 
@@ -11,4 +11,9 @@ data.plot().legend(bbox_to_anchor = (1, 1))
 
 print(data.describe())
 
+plt.xlabel("Årstall")
+plt.ylabel("Minutter per dag i gjennomsnitt")
+plt.title("Tid brukt på ulike medier 2010–2019")
+plt.suptitle("Kilde: Statistisk sentralbyrå", fontsize = 8)
+plt.grid(True)
 plt.show()
