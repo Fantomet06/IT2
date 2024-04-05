78fd61a4ebebabaca04e64c7e11c0b40bbb79061
Fantomet <44312327+Fantomet06@users.noreply.github.com>
2024-04-05 15:04:47 +0200

    update

--- prosjekter/omdb/app/app.py
+++ prosjekter/omdb/app/app.py
@@ -1,33 +1,36 @@
 import sys
 sys.dont_write_bytecode = True #NO PYCHACHE
 
-from flask import Flask, request, render_template 
+from flask import Flask, request, render_template, redirect, url_for
 import backend
 
 app = Flask(__name__) 
 
+@app.route('/result', methods=['GET', 'POST'])
+def result():
+	search = request.args.get('query')
+	film_data = backend.hent_sok(search)
+	text = []
+	for film in film_data:
+		text.append(vars(film))
+	return render_template('namesearch.html', title='Filmsøk', data=text)
 
 @app.route('/', methods=['GET', 'POST']) 
 def index(): 
 	if request.method == 'POST': 
-		# Retrieve the text from the textarea 
+		# retrieve text from the form
 		namesearch = request.form.get('namesearch')
 		idsearch = request.form.get('idsearch')
 
 		if namesearch:
-			# Print the text in terminal for verification 
-			film_data = backend.hent_sok(namesearch)
-			text = ""
-			for film in film_data:
-				text += film.__str__() + 2*"<br>"
-			return text
+			return redirect(url_for('result', query=namesearch))
 		
 		if idsearch:
 			# Print the text in terminal for verification 
 			film_data = backend.hent_film_info(idsearch)
 			return film_data.__str__() + 2*"<br>" + f'<img src="{film_data.poster}" alt="Poster">'
 
-	return render_template('index.html') 
+	return render_template('index.html', title='Filmsøk') 
 
 
 if __name__ == '__main__': 
