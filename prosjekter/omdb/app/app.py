6dd431b62e8392fa89d6450c98e74d93b7441528
Fantomet <44312327+Fantomet06@users.noreply.github.com>
2024-04-09 15:16:57 +0200

    updates

--- prosjekter/omdb/app/app.py
+++ prosjekter/omdb/app/app.py
@@ -3,16 +3,52 @@ sys.dont_write_bytecode = True #NO PYCHACHE
 
 from flask import Flask, request, render_template, redirect, url_for
 import backend
+from backend import Favorites
 
 app = Flask(__name__,template_folder='./frontend/templates',static_folder='./frontend/static') 
 
-@app.route('/result', methods=['GET', 'POST'])
+@app.route('/favorites/add', methods=['GET', 'POST'])
+def add_favorite():
+	imdb_id = request.args.get('imdb_id', None)
+	display = request.args.get('test')
+	if display == 1: # sjekk om det er en redirect fra details
+		return "HEI"
+	film_data = backend.hent_film_info(imdb_id)
+	if film_data == 503:
+		return "Feil ved henting av filminformasjon."
+	
+	# Save to favorites
+	favorites.add_favorite(film_data)
+	return redirect(url_for('favorites'))
+
+@app.route('/favorites', methods=['GET'])
+def favorites():
+	return favorites.__str__()
+
+@app.route('/result', methods=['GET'])
 def result():
 	search = request.args.get('query')
 	film_data = backend.hent_sok(search)
 	movies = [vars(movie) for movie in film_data[0]]
 	series = [vars(serie) for serie in film_data[1]]
-	return render_template('namesearch.html', title='Filmsøk', movies=movies, series=series)
+	return render_template('namesearch.html', title=search, movies=movies, series=series)
+
+@app.route('/details', methods=['GET', 'POST'])
+def details():
+    # Perform any necessary actions here
+    # You can use the button_id to determine which image to display
+	imdb_id = request.args.get('imdb_id')
+	film_data = backend.hent_film_info(imdb_id)
+	if film_data == 503:
+		return "Feil ved henting av filminformasjon."
+	
+	global last_search # Save object in case of add_favorite
+	last_search = film_data # prevent unnecesary API calls
+
+	if film_data.genre == "movie":
+		return render_template('movie.html', data=vars(film_data))
+	else:
+		return render_template('serie.html', data=vars(film_data))
 
 @app.route('/', methods=['GET', 'POST']) 
 def index(): 
@@ -33,4 +69,5 @@ def index():
 
 
 if __name__ == '__main__': 
-	app.run() 
+	favorites = Favorites()
+	app.run()
--- prosjekter/omdb/app/backend.py
+++ prosjekter/omdb/app/backend.py
@@ -9,15 +9,22 @@ def hent_film_info(imdbID):
     """ Hente informasjon om et film"""
     url = settings.url + "&i=" + imdbID
     response = requests.get(url)
-    if response.status_code == 200:  # sjekker at HTTP request til API gikk bra.
-        film_data = response.json()
-        if film_data["Response"] == "False":   #sjekker om OMDb API tjeneste kall gikk bra.
-            print(film_data["Error"])
-            return 
-        return Movie(film_data["Title"], film_data["Year"], film_data["Type"], film_data["imdbID"], film_data["Poster"])
-    else:
+    # sjekker at HTTP request til API gikk bra.
+    if response.status_code != 200:
         print('Feil ved henting av filminformasjon.')
-        return None
+        return 503
+
+    #sjekk at API kall gikk bra
+    film_data = response.json()
+    if film_data["Response"] == "False":
+        print(film_data["Error"])
+        return 503
+    
+    #hvis alt gikk bra
+    if film_data["Type"] == "movie":
+        return Movie(film_data["Title"], film_data["Year"], film_data["imdbID"], film_data["Poster"], film_data["DVD"], film_data["Production"], film_data["Website"], film_data["Type"])
+    elif film_data["Type"] == "series":
+        return Series(film_data["Title"], film_data["Year"], film_data["imdbID"], film_data["Poster"], film_data["totalSeasons"], film_data["Type"])
     
 def hent_sok(tittel) -> list:
     """
@@ -27,33 +34,33 @@ def hent_sok(tittel) -> list:
     """
     url = settings.url + "&s=" + tittel
     response = requests.get(url)
-    if response.status_code == 200:  # sjekker at HTTP request til API gikk bra.
-        film_data = response.json()
-        if film_data["Response"] == "False":   #sjekker om OMDb API tjeneste kall gikk bra.
-            print(film_data["Error"])
-            return 
-        else:
-            results = [[],[]]
-            for i in film_data["Search"]:
-                if i["Type"] == "movie":
-                    results[0].append(Movie(i["Title"], i["Year"], i["imdbID"], i["Poster"]))
-                elif i["Type"] == "series":
-                    results[1].append(Series(i["Title"], i["Year"], i["imdbID"], i["Poster"]))
-            return results
-    else:
-        print('Feil ved henting av filminformasjon.')
-        return None
+    if response.status_code != 200:  # sjekker at HTTP request til API gikk bra.
+        return 'Feil ved henting av filminformasjon.'
+
+    film_data = response.json()
+    if film_data["Response"] == "False":   #sjekker om OMDb API tjeneste kall gikk bra.
+        return film_data["Error"]
+    
+    #hvis alt gikk bra, så sorterer vi filmer og serier
+    results = [[],[]]
+    for film in film_data["Search"]:
+        if film["Type"] == "movie":
+            results[0].append(AudiovisueltElement(film["Title"], film["Year"], film["imdbID"], film["Poster"]))
+        elif film["Type"] == "series":
+            results[1].append(AudiovisueltElement(film["Title"], film["Year"], film["imdbID"], film["Poster"]))
+    return results
     
 class AudiovisueltElement:
-    def __init__(self, title, year, imdb_id, poster):
+    def __init__(self, title, year, imdb_id, poster, genre=None):
         self.title = title
         self.year = year
         self.imdb_id = imdb_id
         self.poster = poster
-        self.id = id
+        self.genre = genre
+        #self.id = id
         self.ratings = [] #TODO
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"""
         Tittel: {self.title}
         År: {self.year}
@@ -63,10 +70,33 @@ class AudiovisueltElement:
 
 class Movie(AudiovisueltElement):
     """ Klasse for å representere en film. """
-    def __init__(self, title, year, imdb_id, poster):
-        super().__init__(title, year, imdb_id, poster)
+    def __init__(self, title, year, imdb_id, poster, DVD, production, website, genre):
+        super().__init__(title, year, imdb_id, poster, genre)
+        self.DVD = DVD
+        self.production = production
+        self.website = website
 
 class Series(AudiovisueltElement):
     """ Klasse for å representere en serie. """
-    def __init__(self, title, year, imdb_id, poster):
-        super().__init__(title, year, imdb_id, poster)
+    def __init__(self, title, year, imdb_id, poster, total_seasons, genre):
+        super().__init__(title, year, imdb_id, poster, genre)
+        self.total_seasons = total_seasons
+
+class Favorites:
+    def __init__(self):
+        self.favorites = [[],[]] # [[Movie], [Series]]
+
+    def add_favorite(self, favorite):
+        if favorite.genre == "movie":
+            self.favorites[0].append(favorite)
+        else:
+            self.favorites[1].append(favorite)
+
+    def remove_favorite(self, favorite):
+        self.favorites.remove(favorite)
+
+    def get_favorites(self):
+        return self.favorites
+
+    def __str__(self):
+        return str([vars(favorite) for favorite in self.favorites])
